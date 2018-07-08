7c1ea7f58c87c3472e4047f1a9f3a69e5a6c92f4
Matt Greenberg <magreenberg@fullsail.edu>
2018-07-08 17:03:15 -0600

    Adding react and react-dom with a basic react file

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+/*----------  Vendor Imports  ----------*/
+import React from 'react';
+import { Render } from 'react-dom';
+
+/*----------  Custom Imports  ----------*/
+
+
+Render(
+  <h1>React Works</h1>,
+  document.getElementById('root'),
+);
