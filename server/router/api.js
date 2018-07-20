22d131c2c3153e837bb6c87c9dde6da7b20c010c
Matt Greenberg <magreenberg@fullsail.edu>
2018-07-19 20:09:36 -0600

    Added a good route to setup a new user, it responsds correctly

--- server/router/api.js
+++ server/router/api.js
@@ -1,6 +1,10 @@
 /*----------  Vendor Imports  ----------*/
 const { Router } = require('express');
 
+/*----------  Custom Imports  ----------*/
+const UserModel = require('../schema/user');
+const { log, hash, verify } = require('../utility');
+
 /*----------  Setup  ----------*/
 const ApiRouter = Router();
 
@@ -8,11 +12,69 @@ const ApiRouter = Router();
 =            ApiRouter            =
 ==================================*/
 
-ApiRouter.get('/', (req, res, next) => {
-
-  res.json({
-    message: 'Hello World!',
+ApiRouter.route('/user')
+  .get((req, res, next) => {
+    res.send('get');
   })
+  .post((req, res, next) => {
+    res.send('post');
+  })
+  .put((req, res, next) => {
+    res.send('put');
+  })
+  .delete((req, res, next) => {
+    res.send('delete');
+  });
+
+ApiRouter.post('/user/signup', (req, res) => {
+
+  // Validate the post PARAMS
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) return res.status(422).send({
+    message: 'Missing Paramaters In Request',
+  });
+
+  // Prep a new user
+  const hashObj = hash(password);
+  const newUser = new UserModel({
+    username,
+    email,
+    salt: hashObj.salt,
+    password: hashObj.hashedPassword,
+  });
+
+  // Save the new user in the database
+  newUser.save()
+    .then((data) => {
+      req.auth.userId = data._id;
+      res.send({
+        username: data.username,
+        email: data.email,
+        message: 'Login OK',
+      });
+    })
+    .catch((err) => {
+
+      // eslint-disable-next-line
+      const regex = /index\:\ (?:.*\.)?\$?(?:([_a-z0-9]*)(?:_\d*)|([_a-z0-9]*))\s*dup key/i;
+
+      // Catch duplicate creation
+      if (err.code === 11000) {
+        const key = err.message.match(regex)[1];
+        const value = (key === 'username') ? username : email;
+        return res.status(409).send({
+          message: `The ${key} '${value}' already exists`,
+          key,
+          value,
+        });
+      }
+
+      // Log and close the response
+      log.error(err.message);
+      res.status(500).send({
+        message: 'Server Error, Oops',
+      });
+    });
 
 });
 
--- server/utility.js
+++ server/utility.js
@@ -1,6 +1,7 @@
 /*----------  Vendor Imports  ----------*/
 const winston = require('winston');
 const path = require('path');
+const crypto = require('crypto');
 
 /**
  * Winston Logger - Can write errors to file
@@ -27,3 +28,32 @@ const logger = winston.createLogger({
 
 // Export Logger as `log`
 module.exports.log = logger;
+
+/**
+ * Crypto stuff, its a secret
+ */
+function hash(password) {
+  if (typeof password !== 'string') throw new Error('You can only hash a string');
+  const output = {};
+  output.salt = crypto.randomBytes(5).toString('hex');
+  output.hashedPassword = password;
+  for(let i=0; i<3; i++){
+    output.hashedPassword = crypto.createHmac('sha256', output.salt).update(output.hashedPassword).digest('hex');
+  }
+  return output;
+}
+
+module.exports.hash = hash;
+
+/**
+ * More crypto, why not
+ */
+function verify(password, salt, hashedPassword) {
+  if (!password || !salt || !hashedPassword) return false;
+  for(let i=0; i<3; i++){
+    password = crypto.createHmac('sha256', salt).update(password).digest('hex');
+  }
+  return (hashedPassword === password);
+}
+
+module.exports.verify = verify;
